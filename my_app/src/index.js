aa9469e098afca498cd81980018620149d8241fb
Tim Combs <tim00combs@gmail.com>
2019-05-04 06:55:38 -0700

    now have working tic-tac-toe game. added a calculateWinner function that looks for line of 3 of the same symbol, refactored Board to check for winner and render player's turn and winner views.

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -16,7 +16,8 @@ class Board extends React.Component {
     super(props);
     this.state = {
       // create array of 9 nulls - corresponds to 9 board squares
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
@@ -24,11 +25,20 @@ class Board extends React.Component {
     // makes shallow copy of arr that represents squares on board
     // to manipulate w/out mutating state
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+
+    // returns early if we have a winner or click on filled square
+    if (calculateWinner(squares) || squares[i]) {return;}
+
+    // puts the correct symbol on square
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      // toggles X or O
+      xIsNext: !this.state.xIsNext
+    });
   }
 
-  renderSquare(i) {
+  renderSquare(i) {  
     return (
       <Square value={this.state.squares[i]}
               onClick={() => this.handleClick(i)} />
@@ -36,7 +46,12 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    winner ?
+      status ='Winner: ' + winner :
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+  
     return (
       <div>
         <div className='status'>{status}</div>
@@ -85,3 +100,24 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
