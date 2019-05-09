f767cc7db42025e94ac7984129bc2732c38ec339
Tim Combs <tim00combs@gmail.com>
2019-05-09 16:41:43 -0700

    Just messing around with installing SVG as the buttons. could make a hacky solution, gonna look at FontAwesome though it is overkill to install all the libaries

--- quote_app/src/index.js
+++ quote_app/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ReactComponent as TwitterLogo} from './twitLogo.svg';
 import './index.css';
 
-/* USER STORIES
-  1: I can see a wrapper element with a corresponding id="quote-box".
-  2: Within #quote-box, I can see an element with a corresponding id="text".
-  3: Within #quote-box, I can see an element with a corresponding id="author".
-  4: Within #quote-box, I can see a clickable element with a corresponding id="new-quote".
-  5: Within #quote-box, I can see a clickable element with a corresponding id="tweet-quote".
-  6: On first load, my quote machine displays a random quote in the element with id="text".
-  7: On first load, my quote machine displays the random quote's author in the element with id="author".
-  8: When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
-  9: My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
-  10: I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in it's href attribute to tweet the current quote.
-  11: The #quote-box wrapper element should be horizontally centered. Please run tests with browser's zoom level at 100% and page maximized.
+/* TODOs
+  1 - add icons for twitter and tumblr buttons
+  2 - color of button should change like the New Quote button text
+  3 - find a cool web font to use
+  4 - figure out how to bundle it for the desolidstate website
+  4.5 - get the newDevCity.io website and bundle it there.
+  5 - do the weird beginning quote thing?
 */
 
 class App extends React.Component {
@@ -124,7 +120,7 @@ function NewQuoteButton(props) {
 function TwitterButton(props) {
   console.log(props);
   return (
-    <button className='button elem-clr1 txt-clr1' name='twitter' onClick={props.onClick}>Twitter</button>
+    <TwitterLogo className='button elem-clr1 txt-clr1' name='twitter' onClick={props.onClick}/>
   );
 }
 
