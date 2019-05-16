7710a273f187d2a531b37fffa2c6978e16bc0c82
Tim Combs <tim00combs@gmail.com>
2019-05-16 16:51:45 -0700

    changed index.css and index.js - refactored handleClick using destructuring on components that used this.state and put the e.target.name into the variable clicked and made the window.open calls better to simplify the code for readability. also refactored getColors to not make DOM calls, but pass class names with color changes through the quote and buttons. had to add a hacky call to dom to manipulate the body with these same classes to keep the color change consistent.

--- quote_app/src/index.js
+++ quote_app/src/index.js
@@ -18,14 +18,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: [{
-        quote: '',
-        author: ''
-      }],
-      currQuote: {
-        quote: '',
-        author: ''
-      },
+      quotes: [],
+      currQuote: {},
       clrScheme: {
         bkg: 'elem-clr1',
         txt: 'txt-clr1'
@@ -37,10 +31,10 @@ class App extends React.Component {
     try {
       const blob = await fetch('https://raw.githubusercontent.com/timcombs/marx-headmon/master/quotes.json');
       const text = await blob.text();
-      const json = await JSON.parse(text);
+      const { quotes } = await JSON.parse(text);
       this.setState({
-        quotes: json.quotes,
-        currQuote: this.getQuote(json.quotes)
+        quotes,
+        currQuote: this.getQuote(quotes)
       });
     }catch(err){
       console.log(err);
@@ -49,15 +43,25 @@ class App extends React.Component {
   }
   
   handleClick(e) {
-    if (e.target.name === 'newquote') {
+    const { quotes, currQuote: { quote, author } } = this.state;
+    const clicked = e.target.name;
+
+    if (clicked === 'newquote') {
       this.setState({
-        currQuote: this.getQuote(this.state.quotes)
+        currQuote: this.getQuote(quotes)
       });
       this.changeColors();
-    }else if (e.target.name === 'twitter') {
-      window.open('https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent(`${this.state.currQuote.quote} -${this.state.currQuote.author}`), '_blank');
-    }else if (e.target.name === 'tumblr') {
-    window.open('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + encodeURIComponent(this.state.currQuote.author) + '&content=' + encodeURIComponent(this.state.currQuote.quote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button', '_blank');
+    }else if (clicked === 'twitter') {
+      window.open('https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+                  encodeURIComponent(
+                  `${quote} -${author}`),
+                  '_blank');
+    }else if (clicked === 'tumblr') {
+    window.open('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+                encodeURIComponent(author) + '&content=' +
+                encodeURIComponent(quote) +
+                '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button',
+                '_blank');
     }
 
   }
@@ -69,41 +73,28 @@ class App extends React.Component {
   }
 
   changeColors() {
-    const buttons = document.querySelectorAll('.button');
-    const bkg = document.querySelector('body');
-    const quoteText = document.querySelector('.quote');
-
     const rnd = Math.floor(Math.random() * 4) + 1;
-
-    buttons.forEach((button) => {
-      button.classList.replace(this.state.clrScheme.bkg, `elem-clr${rnd}`);
-      button.classList.replace(this.state.clrScheme.txt, `txt-clr${rnd}`);
-    });
-
-    bkg.classList.contains(this.state.clrScheme.bkg) ?
-      bkg.classList.replace(this.state.clrScheme.bkg, `elem-clr${rnd}`) :
-      bkg.classList.add(`elem-clr${rnd}`);
-    
-    quoteText.classList.replace(this.state.clrScheme.txt, `txt-clr${rnd}`);
 
     this.setState({
       clrScheme: {
         bkg: `elem-clr${rnd}`,
         txt: `txt-clr${rnd}`
       }
-    })
+    });
 
   }
 
   render() {
-    console.log(window.innerWidth);
+    // hack to change background color of body
+    document.body.classList = '';
+    document.body.classList.add(this.state.clrScheme.bkg);
     return (
-      <div>
-        <section className='card'>
-          <Quote currQuote={this.state.currQuote} />
-          <TwitterButton onClick={(e) => this.handleClick(e)} />
-          <TumblrButton onClick={(e) => this.handleClick(e)} />
-          <NewQuoteButton onClick={(e) => this.handleClick(e)} />
+      <div className={`app ${this.state.clrScheme.bkg}`}>
+        <section className='card' >
+          <Quote clr={this.state.clrScheme} currQuote={this.state.currQuote} />
+          <TwitterButton clr={this.state.clrScheme} onClick={(e) => this.handleClick(e)} />
+          <TumblrButton clr={this.state.clrScheme} onClick={(e) => this.handleClick(e)} />
+          <NewQuoteButton clr={this.state.clrScheme} onClick={(e) => this.handleClick(e)} />
         </section>
       </div>
     );
@@ -112,26 +103,26 @@ class App extends React.Component {
 
 function NewQuoteButton(props) {
   return (
-    <button className='button getquote elem-clr1 txt-clr1' name='newquote' onClick={props.onClick}>New Quote</button>
+    <button className={`button getquote ${props.clr.bkg} ${props.clr.txt}`} name='newquote' onClick={props.onClick}>New Quote</button>
   );
 }
 
 function TwitterButton(props) {
   return (
-    <button className='button social elem-clr1 txt-clr1' name='twitter' onClick={props.onClick}><FontAwesomeIcon icon={['fab', 'twitter']} /></button>
+    <button className={`button social ${props.clr.bkg} ${props.clr.txt}`} name='twitter' onClick={props.onClick}><FontAwesomeIcon icon={['fab', 'twitter']} /></button>
   );
 }
 
 function TumblrButton(props) {
   return (
-    <button className='button social elem-clr1 txt-clr1' name='tumblr' onClick={props.onClick}><FontAwesomeIcon icon={['fab', 'tumblr']} /></button>
+    <button className={`button social ${props.clr.bkg} ${props.clr.txt}`} name='tumblr' onClick={props.onClick}><FontAwesomeIcon icon={['fab', 'tumblr']} /></button>
   );
 }
 
 function Quote(props) {
   return (
     <div>
-      <p className='quote txt-clr1'>“{props.currQuote.quote}”</p>
+      <p className={`quote ${props.clr.txt}`}>“{props.currQuote.quote}”</p>
       <p className='author'>- {props.currQuote.author}</p>
     </div>
   );
