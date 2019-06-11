43d18ae79b352869ae414d087e20d8e0f1f8f0eb
Tim Combs <tim00combs@gmail.com>
2019-06-11 08:10:47 -0700

    refactored App.js now displays the built in todo items - lots more to do

--- todo_hooks/src/App.js
+++ todo_hooks/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Todo = ({ todo }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo'>{todo.text}</div>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn about React' },
+    { text: 'Meet friend for lunch' },
+    { text: 'Build really neat todo app' }
+  ]);
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
