4626f331604afb53e9d0344c50472cb9b590ba1a
Tim Combs <tim00combs@gmail.com>
2019-06-11 16:57:54 -0700

    can add additional todos. working on deleting them

--- todo_hooks/src/App.js
+++ todo_hooks/src/App.js
@@ -1,18 +1,56 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+          type='text'
+          className='input'
+          value={value}
+          placeholder='Add Todo...'
+          onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 const Todo = ({ todo }) => {
   return (
-    <div className='todo'>{todo.text}</div>
+    <div
+        style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
+        className='todo'>{todo.text}</div>
   );
 }
 
 function App() {
   const [todos, setTodos] = useState([
-    { text: 'Learn about React' },
-    { text: 'Meet friend for lunch' },
-    { text: 'Build really neat todo app' }
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+    },
+    { text: 'Build really neat todo app',
+      isCompleted: false
+    }
   ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
 
   return (
     <div className='app'>
@@ -20,6 +58,7 @@ function App() {
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
