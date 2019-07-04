07f893efb1e9ebe6222d13d04afeadc91b1f6f91
Tim Combs <tim00combs@gmail.com>
2019-07-03 17:02:50 -0700

    using React.createElement instead of the JSX syntactic sugar

--- udemy_app/src/App.js
+++ udemy_app/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className='App'>
-        <h1>Hi, I'm a react application</h1>
-      </div>
-    );
+    // return (
+    //   <div className='App'>
+    //     <h1>yô̟̔̈ͯ̒ur͚̰̠̲ͫ̃̀̇̀͋ friend ̪͌͑ͨͅis ̎͆ͪ̽͒Za͙͉̣͗͌ͫ͒̐́ͯlg̖͖̝̰͑͋͊́̎̈́͗̚o ͉̥̗̣̦̈́̌̎̆ͯ̾te̎ͩ̑ͤ̈́̄xt</h1>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'yô̟̔̈ͯ̒ur͚̰̠̲ͫ̃̀̇̀͋ friend ̪͌͑ͨͅis ̎͆ͪ̽͒Za͙͉̣͗͌ͫ͒̐́ͯlg̖͖̝̰͑͋͊́̎̈́͗̚o ͉̥̗̣̦̈́̌̎̆ͯ̾te̎ͩ̑ͤ̈́̄xt'));
   }
 }
 
