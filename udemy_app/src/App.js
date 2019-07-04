2194f9ff4beab9981d4bd0285e1209be719280b8
Tim Combs <tim00combs@gmail.com>
2019-07-03 17:54:14 -0700

    working with props and props.children - this allows putting jsx between opening closing component tags and then accessing it as props to render

--- udemy_app/src/App.js
+++ udemy_app/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className='App'>
-    //     <h1>yô̟̔̈ͯ̒ur͚̰̠̲ͫ̃̀̇̀͋ friend ̪͌͑ͨͅis ̎͆ͪ̽͒Za͙͉̣͗͌ͫ͒̐́ͯlg̖͖̝̰͑͋͊́̎̈́͗̚o ͉̥̗̣̦̈́̌̎̆ͯ̾te̎ͩ̑ͤ̈́̄xt</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'yô̟̔̈ͯ̒ur͚̰̠̲ͫ̃̀̇̀͋ friend ̪͌͑ͨͅis ̎͆ͪ̽͒Za͙͉̣͗͌ͫ͒̐́ͯlg̖͖̝̰͑͋͊́̎̈́͗̚o ͉̥̗̣̦̈́̌̎̆ͯ̾te̎ͩ̑ͤ̈́̄xt'));
+    return (
+      <div className='App'>
+        <h1>yô̟̔̈ͯ̒ur͚̰̠̲ͫ̃̀̇̀͋ friend ̪͌͑ͨͅis̎͆ͪ̽͒Za͙͉̣͗͌ͫ͒̐́ͯlg̖͖̝̰͑͋͊́̎̈́͗̚o ͉̥̗̣̦̈́̌̎̆ͯ̾te̎ͩ̑ͤ̈́̄xt</h1>
+        <p>but this is not</p>
+        <Person name='Frida' age='104'/>
+        <Person name='Bob' age='84'/>
+        <Person name='LaBradford' age='14'>Hobbies: none</Person>
+        <Person name='unknown' age='infinite'/>
+      </div>
+    );
   }
 }
 
