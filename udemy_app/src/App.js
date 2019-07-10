4d888d50a6752208d72e66042b4f74d10e7fff5a
Tim Combs <tim00combs@gmail.com>
2019-07-10 08:03:02 -0700

    cleaning up commits with udemy tutorial apps

--- udemy_app/src/App.js
+++ udemy_app/src/App.js
@@ -4,15 +4,39 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Frida', age: 104 },
+      { name: 'Bob', age: 84 },
+      { name: 'LaBradford', age: 14 },
+      { name: 'unknown', age: 'infinite' }
+    ]
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>yô̟̔̈ͯ̒ur͚̰̠̲ͫ̃̀̇̀͋ friend ̪͌͑ͨͅis̎͆ͪ̽͒Za͙͉̣͗͌ͫ͒̐́ͯlg̖͖̝̰͑͋͊́̎̈́͗̚o ͉̥̗̣̦̈́̌̎̆ͯ̾te̎ͩ̑ͤ̈́̄xt</h1>
         <p>but this is not</p>
-        <Person name='Frida' age='104'/>
-        <Person name='Bob' age='84'/>
-        <Person name='LaBradford' age='14'>Hobbies: none</Person>
-        <Person name='unknown' age='infinite'/>
+        <br></br>
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >Hobbies: none
+        </Person>
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
       </div>
     );
   }
--- udemy_app/src/Person/Person.js
+++ udemy_app/src/Person/Person.js
@@ -3,10 +3,8 @@ import React from 'react';
 const Person = (props) => {
   return (
     <div>
-      <p>
-        I'm a {props.name} and I am {props.age} years old!
-        <div>{props.children}</div>
-      </p>
+      <p>I'm a {props.name} and I am {props.age} years old!</p>
+      <div>{props.children}</div>
     </div>
   );
 }
