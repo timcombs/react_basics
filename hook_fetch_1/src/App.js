ade98a4b121b9e0c665c58cc81cab227ddb4f481
Tim Combs <tim00combs@gmail.com>
2019-07-11 08:10:30 -0700

    adding code to fetch data using useState and useEffect. issues with CORS

--- hook_fetch_1/src/App.js
+++ hook_fetch_1/src/App.js
@@ -1,6 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
+
+function DataLoader() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10', {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/plain",
+        "mode" : "no-cors"
+    }})
+    .then((response) => response.json())
+    .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {data.map((el) => (
+          <li key={el.id}>{el.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   const [buttonText, setButtonText] = useState('Click me, please');
@@ -9,7 +34,12 @@ function App() {
     return setButtonText('Thanks, been clicked!');
   }
   
-  return <button onClick={handleClick}>{buttonText}</button>
+  return (
+    <div>
+      <button onClick={handleClick}>{buttonText}</button>
+      <DataLoader />
+    </div>
+  );
 }
 
 export default App;
