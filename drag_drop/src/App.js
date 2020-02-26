a171768e422dc27affa852cc060c25ae2d499e93
Tim Combs <tim00combs@gmail.com>
2020-02-26 13:12:41 -0800

    DragAndDrop component done - all 4 drag functions are coded. but don't yet render what is in the file list.

--- drag_drop/src/App.js
+++ drag_drop/src/App.js
@@ -4,10 +4,27 @@ import DragAndDrop from './DragAndDrop';
 import './App.css';
 
 function App() {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_DROP_DEPTH':
+        return { ...state, dropDepth: action.dropDepth };
+      case 'SET_IN_DROP_ZONE':
+        return { ...state, inDropZone: action.inDropZone };
+      case 'ADD_FILE_TO_LIST':
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state;
+    }
+  };
+
+  const [data, dispatch] = React.useReducer(
+    reducer, { dropDepth: 0, inDropZone: false, fileList: [] }
+  )
+
   return (
     <div className='App'>
       <h1>React drag-n-drop component</h1>
-      <DragAndDrop />
+      <DragAndDrop data={data} dispatch={dispatch} />
     </div>
   );
 }
--- drag_drop/src/DragAndDrop.js
+++ drag_drop/src/DragAndDrop.js
@@ -1,24 +1,49 @@
 import React from 'react';
 
 const DragAndDrop = props => {
+  const { data, dispatch } = props;
+
   const handleDragEnter = (e) => {
     e.preventDefault();
     e.stopPropagation();
+    dispatch({ type: 'SET_DROP_DEPTH', dropDepth: data.dropDepth + 1 })
   };
 
   const handleDragLeave = (e) => {
     e.preventDefault();
     e.stopPropagation();
-  };
 
+    dispatch({ type: 'SET_DROP_DEPTH', dropDepth: data.dropDepth - 1 });
+
+    if (data.dropDepth > 0) return;
+
+    dispatch({ type: 'SET_IN_DROP_ZONE', inDropZone: false });
+    e.dataTransfer.dropEffect = 'none';
+  };
+  
   const handleDragOver = (e) => {
     e.preventDefault();
     e.stopPropagation();
+    e.dataTransfer.dropEffect = 'copy';
+
+    dispatch({ type: 'SET_IN_DROP_ZONE', inDropZone: true });
   };
 
   const handleDrop = (e) => {
     e.preventDefault();
     e.stopPropagation();
+    let files = [...e.dataTransfer.files];
+
+    if (files && files.length > 0) {
+      const existingFiles = data.fileList.map((f) => f.name);
+      files = files.filter((f) => !existingFiles.includes(f.name));
+
+      dispatch({ type: 'ADD_FILE_TO_LIST', files });
+      e.dataTransfer.clearData();
+
+      dispatch({ type: 'SET_DROP_DEPTH', dropDepth: 0 });
+      dispatch({ type: 'SET_IN_DROP_ZONE', InDropZone: false });
+    }
   };
 
   return (
